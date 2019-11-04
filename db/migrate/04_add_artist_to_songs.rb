1b34bf332e4f0218cea37ce0af10094db787178c
Ryan S <planetlucid@gmail.com>
2019-11-04 23:44:38 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,5 +1,5 @@
-class AddArtistToSongs < ActiveRecord::Migration
-  def change
-    add_column :songs, :artist_id, :integer
-  end
-end
+# class AddArtistToSongs < ActiveRecord::Migration
+#   def change
+#     add_column :songs, :artist_id, :integer
+#   end
+# end
